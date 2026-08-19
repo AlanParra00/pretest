bf90f9b1a5f45610fcfcc49405d123ad2924ef4e
John Hightower <john.hightower@uafs.edu>
2026-08-19 08:45:10 -0500

    Updated

---
+++ pretest/prob1/Prob1.java
@@ -0,0 +1,10 @@
+package prob1;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ pretest/prob3/Prob3.java
@@ -0,0 +1,9 @@
+package prob3;
+
+public class Prob3 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
