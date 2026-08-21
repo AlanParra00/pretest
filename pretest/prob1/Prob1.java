10b6a4b60047db74779e311615e9d1eb1e16da1b
Alan Parra <aparra00@uafs.edu>
2026-08-21 09:47:55 -0500

    Uafs precourse

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -1,10 +1,19 @@
 package prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 
-	public static void main(String[] args) {
-		System.out.printf("Hello World\n");
+	public static void main(int[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+	 int[] Arr = {4, 7, 2, 9, 1};
+	 
+	 
 
+	}
+	public static void findMaxDifference(int [] Arr) {
+		
 	}
 
 }
